0e7f486271fa5f1a6c66cf02bf3261f947ae2dbb
Rudi Grinberg <rudi.grinberg@gmail.com>
2017-05-05 00:41:59 -0400

    remove all unused code in TestUtilities

--- server/src/test/java/com/clarkparsia/pellet/server/protege/TestUtilities.java
+++ server/src/test/java/com/clarkparsia/pellet/server/protege/TestUtilities.java
@@ -1,21 +1,12 @@
 package com.clarkparsia.pellet.server.protege;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.logging.Logger;
 
 /**
  * @author Edgar Rodriguez-Diaz
  */
 public class TestUtilities {
-	private static Logger logger = Logger.getLogger(TestUtilities.class.getCanonicalName());
-
 	public static final String BASE = "file:test/data/";
-
-	public static final File SERVER_ROOT = new File("target/server-distribution/server");
-	public static final File ROOT_DIRECTORY = new File(SERVER_ROOT, "root");
-	public static final File CONFIGURATION_DIRECTORY = new File(SERVER_ROOT, "configuration");
-	public static final File PELLET_DIRECTORY = new File(SERVER_ROOT, "pellet");
 
 	public static final String PROTEGE_HOST = "localhost";
 	public static final String PROTEGE_PORT = "8080";
@@ -55,28 +46,5 @@ public class TestUtilities {
 			}
 		}
 		f.delete();
-	}
-
-	/**
-	 * This routine creates a temporary directory and then creates a file inside that directory.
-	 * <p/>
-	 * This routine is sometimes needed when running a test that needs a temporary file but then also needs to
-	 * be able to write to the containing directory.  When we created the temporary file with File.createTempFile(),
-	 * it was noticed that when certain tests (e.g. the tests that serialize server side ontologies) were run by different
-	 * users the second run would sometimes fail because the second user would fail to have write access to all the contents of
-	 * the containing directory (e.g. the .owlserver directory).
-	 *
-	 * @param name
-	 * @return
-	 * @throws IOException
-	 */
-	public static File createFileInTempDirectory(String name) throws IOException {
-		File tmpDirectory = File.createTempFile("Save", "test");
-		tmpDirectory.delete();
-		if (!tmpDirectory.mkdir()) {
-			throw new IOException("Coud not create temporary directory " + tmpDirectory);
-		}
-		logger.info("Created temporary directory " + tmpDirectory + " for the file " + name);
-		return new File(tmpDirectory, name);
 	}
 }
