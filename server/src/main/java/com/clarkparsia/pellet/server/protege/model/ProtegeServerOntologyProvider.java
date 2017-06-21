0ffd6884284c32085fc6eb5e6b4266fe85fecc43
Rudi Grinberg <rudi.grinberg@gmail.com>
2017-06-20 23:09:17 -0400

    Fetch ontologies from protege
    Add a new implementation for the ontology fetcher. This implementation will use
    the classifiable endpoint in protege to fetch the classifiable project id's.

---
+++ server/src/main/java/com/clarkparsia/pellet/server/protege/model/ProtegeServerOntologyProvider.java
@@ -0,0 +1,34 @@
+package com.clarkparsia.pellet.server.protege.model;
+
+import com.clarkparsia.pellet.server.Configuration;
+import com.clarkparsia.pellet.server.ConfigurationReader;
+import com.clarkparsia.pellet.server.protege.ProtegeServiceUtils;
+import edu.stanford.protege.metaproject.api.Project;
+import org.protege.editor.owl.client.api.Client;
+
+import javax.inject.Inject;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by rgrinberg on 6/20/17.
+ */
+public class ProtegeServerOntologyProvider implements OntologyProvider {
+
+	private final Client client;
+
+	@Inject
+	public ProtegeServerOntologyProvider(Configuration config) {
+		ConfigurationReader theConfigReader = ConfigurationReader.of(config);
+		this.client = ProtegeServiceUtils.connect(theConfigReader);
+	}
+
+	@Override
+	public Set<String> classifiableProjects() {
+		Set<String> projects = new HashSet<>();
+		for (Project project : client.classifiableProjects()) {
+			projects.add(project.getId().get());
+		}
+		return projects;
+	}
+}
--- server/src/test/java/com/clarkparsia/pellet/server/TestModule.java
+++ server/src/test/java/com/clarkparsia/pellet/server/TestModule.java
@@ -1,8 +1,11 @@
 package com.clarkparsia.pellet.server;
 
 import com.clarkparsia.pellet.server.protege.TestProtegeServerConfiguration;
+import com.clarkparsia.pellet.server.protege.model.ConfigurationOntologyProvider;
+import com.clarkparsia.pellet.server.protege.model.OntologyProvider;
 import com.google.inject.AbstractModule;
 import com.google.inject.Module;
+import com.google.inject.Singleton;
 
 import java.util.List;
 
@@ -19,5 +22,6 @@ public class TestModule extends AbstractModule implements Module {
 	@Override
 	protected void configure() {
 		binder().bind(Configuration.class).toInstance(new TestProtegeServerConfiguration(ontologies));
+		binder().bind(OntologyProvider.class).to(ConfigurationOntologyProvider.class).in(Singleton.class);
 	}
 }
